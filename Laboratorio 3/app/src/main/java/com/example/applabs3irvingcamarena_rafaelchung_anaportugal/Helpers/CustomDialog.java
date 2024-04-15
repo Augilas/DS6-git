9d9756a6d32cefd163e94e5b2908c34b4d406b26
Augilas <augilas11@hotmail.com>
2024-04-14 21:39:31 -0500

    corregido lab 3
    pendiente el relative layout y para impedir dividir entre 0

--- Laboratorio 3/app/src/main/java/com/example/applabs3irvingcamarena_rafaelchung_anaportugal/Helpers/CustomDialog.java
+++ Laboratorio 3/app/src/main/java/com/example/applabs3irvingcamarena_rafaelchung_anaportugal/Helpers/CustomDialog.java
@@ -9,24 +9,24 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class CustomDialog extends DialogFragment {
-    private String message;
+    private String titulo;
+    private float resultado;
 
-    public CustomDialog(String message) {
-        this.message = message;
+    public CustomDialog(String titulo, float resultado) {
+        this.titulo = titulo;
+        this.resultado = resultado;
     }
 
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-        builder.setMessage(message)
-                .setPositiveButton("Ok",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogInterface, int id) {
-                                dialogInterface.cancel();
-                            }
-                        });
-
+        builder.setTitle(titulo)
+                .setMessage("El resultado de la operación es = " + Float.toString(resultado))
+                .setPositiveButton("Entendido", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
         return builder.create();
     }
 }
--- Laboratorio 3/app/src/main/java/com/example/applabs3irvingcamarena_rafaelchung_anaportugal/MainActivity.java
+++ Laboratorio 3/app/src/main/java/com/example/applabs3irvingcamarena_rafaelchung_anaportugal/MainActivity.java
@@ -1,84 +1,104 @@
 package com.example.applabs3irvingcamarena_rafaelchung_anaportugal;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.applabs3irvingcamarena_rafaelchung_anaportugal.Helpers.CustomDialog;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     //declaramos las variables de los controles que vamos a utilizar
     EditText txtNum1;
     EditText txtNum2;
-    RadioGroup radioGroup;
+    RadioGroup radiGroup;
     TextView lblResultado;
+
+    CheckBox cDialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EdgeToEdge.enable(this);
         this.InicializarControles();
     }
 
     private void InicializarControles() {
         txtNum1 = (EditText)findViewById(R.id.txtNum1);
         txtNum2 = (EditText)findViewById(R.id.txtNum2);
-        radioGroup = findViewById(R.id.radioGroup);
+        radiGroup = (RadioGroup) findViewById(R.id.radioGroup);
         lblResultado = (TextView)findViewById(R.id.lblRes);
+        cDialogo = (CheckBox)findViewById(R.id.dialog);
     }
 
-    public void calcularOperacion(View view) {
+    public void calcularOperaciones(View view) {
+        int resultado = 0;
         try {
-            int n1 = Integer.parseInt(txtNum1.getText().toString());
-            int n2 = Integer.parseInt(txtNum2.getText().toString());
-            int resultado = 0;
+            //El controlador de radio
+            int IdBotonRadio = radiGroup.getCheckedRadioButtonId();
+            RadioButton botonRad = findViewById(IdBotonRadio);
+            if (botonRad != null) {
+                String selector = botonRad.getText().toString();
+                switch (selector) {
+                    case "Suma":
+                        resultado = Integer.parseInt(txtNum1.getText().toString()) + Integer.parseInt(txtNum2.getText().toString());
+                        Dialogo(selector, resultado, cDialogo.isChecked());
+                        break;
+                    case "Resta":
+                        resultado = Integer.parseInt(txtNum1.getText().toString()) - Integer.parseInt(txtNum2.getText().toString());
+                        Dialogo(selector, resultado, cDialogo.isChecked());
+                        break;
+                    case "Division":
+                        if (Integer.parseInt(txtNum2.getText().toString()) != 0) {
+                            resultado = Integer.parseInt(txtNum1.getText().toString()) / Integer.parseInt(txtNum2.getText().toString());
+                        } else {
+                            Toast.makeText(this, "No se puede dividir por cero. Por favor, intente de nuevo", Toast.LENGTH_SHORT).show();
+                            resultado = 0;
+                        }
 
-            // Check which radio button is checked and perform the corresponding operation
-            int selectedId = radioGroup.getCheckedRadioButtonId();
-            switch (selectedId) {
-                case R.id.radioSuma:
-                    resultado = sumarNumeros(n1, n2);
-                    break;
-                case R.id.radioResta:
-                    resultado = restarNumeros(n1, n2);
-                    break;
-                case R.id.radioMultiplicacion:
-                    resultado = multiplicarNumeros(n1, n2);
-                    break;
-                case R.id.radioDivision:
-                    resultado = dividirNumeros(n1, n2);
-                    break;
-            }
+                        Dialogo(selector, resultado, cDialogo.isChecked());
+                        break;
+                    case "Multiplicacion":
+                        resultado = Integer.parseInt(txtNum1.getText().toString()) * Integer.parseInt(txtNum2.getText().toString());
+                        Dialogo(selector, resultado, cDialogo.isChecked());
+                        break;
+                    default:
+                        resultado = Integer.parseInt(txtNum1.getText().toString()) % Integer.parseInt(txtNum2.getText().toString());
+                        Dialogo(selector, resultado, cDialogo.isChecked());
+                        break;
+                }
+            }  else {
+            Toast.makeText(this, "Ninguna opción ha sido seleccionada. Por favor, escoja alguna de las operaciones disponibles", Toast.LENGTH_SHORT).show();
+        }
 
-            // Set the result to the TextView
-            lblResultado.setText(String.valueOf(resultado));
         } catch (NumberFormatException e) {
             Toast.makeText(this, "Por favor, ingrese valores válidos", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private int sumarNumeros(int n1, int n2) {
-        return n1 + n2;
-    }
 
-    private int restarNumeros(int n1, int n2) {
-        return n1 - n2;
-    }
-
-    private int multiplicarNumeros(int n1, int n2) {
-        return n1 * n2;
-    }
-
-    private int dividirNumeros(int n1, int n2) {
-        if (n2 != 0) {
-            return n1 / n2;
+    protected void Dialogo(String titulo, float resultado, Boolean res) {
+        if(res) {
+            lblResultado.setText("");
+            CustomDialog dialog = new CustomDialog(titulo, resultado);
+            dialog.show(getSupportFragmentManager(), "dialogo");
         } else {
-            Toast.makeText(this, "No se puede dividir por cero. Por favor, intente de nuevo", Toast.LENGTH_SHORT).show();
-            return 0;
+            lblResultado.setText(String.format(Locale.getDefault(), "%.3f", resultado));
         }
     }
 }
