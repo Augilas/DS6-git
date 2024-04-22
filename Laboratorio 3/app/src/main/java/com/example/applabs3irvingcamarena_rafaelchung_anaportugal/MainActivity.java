3bf5cc0c14930db0b380d74dbaa57c31b2a0786b
Rafael Chung <rafaelchung28@hotmail.com>
2024-04-21 22:51:44 -0500

    wah

--- Laboratorio 3/app/src/main/java/com/example/applabs3irvingcamarena_rafaelchung_anaportugal/MainActivity.java
+++ Laboratorio 3/app/src/main/java/com/example/applabs3irvingcamarena_rafaelchung_anaportugal/MainActivity.java
@@ -1,34 +1,49 @@
 package com.example.applabs3irvingcamarena_rafaelchung_anaportugal;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.applabs3irvingcamarena_rafaelchung_anaportugal.Helpers.CustomDialog;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    //declaramos las variables de los controles que vamos a utilizar
-    EditText txtNum1;
-    EditText txtNum2;
-    RadioGroup radioGroup;
+    // declaramos las variables de los controles que vamos a utilizar
+    EditText txtNum1, txtNum2;
+    RadioButton radioSuma, radioResta, radioMultiplicacion, radioDivision;
+    Button btnCalcular, btnDiologResultado;
     TextView lblResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EdgeToEdge.enable(this);
         this.InicializarControles();
     }
 
     private void InicializarControles() {
-        txtNum1 = (EditText)findViewById(R.id.txtNum1);
-        txtNum2 = (EditText)findViewById(R.id.txtNum2);
-        radioGroup = findViewById(R.id.radioGroup);
-        lblResultado = (TextView)findViewById(R.id.lblRes);
+        txtNum1 = (EditText) findViewById(R.id.txtNum1);
+        txtNum2 = (EditText) findViewById(R.id.txtNum2);
+        radioSuma = findViewById(R.id.radioSuma);
+        radioResta = findViewById(R.id.radioResta);
+        radioMultiplicacion = findViewById(R.id.radioMultiplicacion);
+        radioDivision = findViewById(R.id.radioDivision);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        lblResultado = (TextView) findViewById(R.id.lblRes);
     }
 
     public void calcularOperacion(View view) {
@@ -37,25 +52,27 @@ public class MainActivity extends AppCompatActivity {
             int n2 = Integer.parseInt(txtNum2.getText().toString());
             int resultado = 0;
 
-            // Check which radio button is checked and perform the corresponding operation
-            int selectedId = radioGroup.getCheckedRadioButtonId();
-            switch (selectedId) {
-                case R.id.radioSuma:
-                    resultado = sumarNumeros(n1, n2);
-                    break;
-                case R.id.radioResta:
-                    resultado = restarNumeros(n1, n2);
-                    break;
-                case R.id.radioMultiplicacion:
-                    resultado = multiplicarNumeros(n1, n2);
-                    break;
-                case R.id.radioDivision:
-                    resultado = dividirNumeros(n1, n2);
-                    break;
+            if (radioSuma.isChecked()) {
+                resultado = sumarNumeros(n1, n2);
+                Dialogo(resultado);
             }
-
-            // Set the result to the TextView
-            lblResultado.setText(String.valueOf(resultado));
+            if (radioResta.isChecked()) {
+                resultado = restarNumeros(n1, n2);
+                Dialogo(resultado);
+            }
+            if (radioMultiplicacion.isChecked()) {
+                resultado = multiplicarNumeros(n1, n2);
+                Dialogo(resultado);
+            }
+            if (radioDivision.isChecked()) {
+                resultado = dividirNumeros(n1, n2);
+                Dialogo(resultado);
+            } else {
+                Toast.makeText(this,
+                        "Ninguna opción ha sido seleccionada. Por favor, escoja alguna de las operaciones disponibles",
+                        Toast.LENGTH_SHORT).show();
+            }
+            
         } catch (NumberFormatException e) {
             Toast.makeText(this, "Por favor, ingrese valores válidos", Toast.LENGTH_SHORT).show();
         }
@@ -77,8 +94,20 @@ public class MainActivity extends AppCompatActivity {
         if (n2 != 0) {
             return n1 / n2;
         } else {
-            Toast.makeText(this, "No se puede dividir por cero. Por favor, intente de nuevo", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "No se puede dividir por cero. Por favor, intente de nuevo", Toast.LENGTH_SHORT)
+                    .show();
             return 0;
         }
     }
+
+    protected void Dialogo(String titulo, float resultado, Boolean res) {
+        if (res) {
+            lblResultado.setText("");
+            CustomDialog dialog = new CustomDialog(titulo, resultado);
+            dialog.show(getSupportFragmentManager(), "dialogo");
+        } else {
+            lblResultado.setText(String.format(Locale.getDefault(), "%.3f", resultado));
+        }
+    }
+
 }
